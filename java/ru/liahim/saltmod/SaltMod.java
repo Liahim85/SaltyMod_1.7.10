5c27d01c9d1f02121ce38865488c2a0279a32f14
Liahim85 <mirage85@inbox.ru>
2016-04-10 08:23:39 +0300

    Added files via upload

--- java/ru/liahim/saltmod/SaltMod.java
+++ java/ru/liahim/saltmod/SaltMod.java
@@ -1,47 +1,54 @@
 package ru.liahim.saltmod;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import ru.liahim.saltmod.common.CommonProxy;
+import ru.liahim.saltmod.init.ModBlocks;
+import ru.liahim.saltmod.init.ModItems;
+import ru.liahim.saltmod.init.SaltConfig;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid=SaltMod.MODID, name=SaltMod.NAME, version=SaltMod.VERSION)
 
 public class SaltMod {
-	
+
 	public static final String MODID = "SaltMod";
 	public static final String NAME = "Salty Mod";
-	public static final String VERSION = "1.7.d";
-	
-	@Instance(MODID)
+	public static final String VERSION = "1.8.a";
+	public static final Logger logger = LogManager.getLogger(NAME);
+
+    public static SaltConfig config;
+
+	@Mod.Instance(MODID)
 	public static SaltMod instance;
 
-	@SidedProxy(clientSide="ru.liahim.saltmod.client.ClientProxy", serverSide="ru.liahim.saltmod.common.CommonProxy")
+	@SidedProxy(clientSide = "ru.liahim.saltmod.common.ClientProxy", serverSide = "ru.liahim.saltmod.common.CommonProxy")
 	public static CommonProxy proxy;
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event)
-	{
-		this.proxy.preInit(event);
-	}
-	
-	@EventHandler
-	public void init(FMLInitializationEvent event)
-	{
-		this.proxy.init(event);
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger.info("Starting SaltMod PreInitialization");
+        config = new SaltConfig(event.getSuggestedConfigurationFile());
+        config.preInit();
+        ModItems.init();
+        ModBlocks.init();
+		proxy.preInit(event);
 	}
 
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event)
-	{
-		this.proxy.postInit(event);
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+        config.init();
+		proxy.init(event);
 	}
-    
-	@EventHandler
-	public void serverStarting(FMLServerStartingEvent event){}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+        config.postInit();
+		proxy.postInit(event);
+	}
 }
